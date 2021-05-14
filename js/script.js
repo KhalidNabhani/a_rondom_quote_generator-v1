1e7b5b190d9cb8211ec93b7bd4ed2cb71eca4f0f
KhalidNabhani <49943723+KhalidNabhani@users.noreply.github.com>
2021-05-14 10:33:24 +0400

    First update

--- js/script.js
+++ js/script.js
@@ -10,19 +10,50 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+const quotes =[
+  {
+    quote :"Avoid sitting on the paths. They (the Companions) said: Allah's Messenger, \
+    we cannot help but holding our meetings (in these paths) and discuss matters (there). \
+    Thereupon Allah's Messenger (ﷺ) said: If you insist on holding meetings, then give the path its due right.\
+    They said: What are its due rights? \
+    Upon this he said: Lowering the gaze, refraining from doing harm, exchanging of greetings.\
+     commanding of good and forbidding from evil",
+    source:"Prophet Mohammed (ﷺ)",
+    citation:"USC-MSA web (English) reference	",
+    year:""
+  },
+  {
+    quote :"Since when you have taken people for slaves and they were born free",
+    source:"Omar ibn Al-Khattab",
+    citation:"good reads - https://www.goodreads.com/author/quotes/5280158.Umar_ibn_Al_Khattab",
+    year:""
+  },
+  {
+    quote :"Whosoever shows you your faults is your friend. Those that pay you lip service in praise are your executioners.",
+    source:"Omar ibn Al-Khattab",
+    citation:"good reads - https://www.goodreads.com/author/quotes/5280158.Umar_ibn_Al_Khattab",
+    year:""
+  },
 
+]
 
 
 /***
  * `getRandomQuote` function
 ***/
 
+function  getRandomQuote (){
+
+}
+
 
 
 /***
  * `printQuote` function
 ***/
+function  printQuote(){
 
+}
 
 
 /***
